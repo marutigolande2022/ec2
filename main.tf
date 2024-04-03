60ae9b21a3ce83acf4299fbb59f3d63b57e3bcf8
marutigolande2022 <163817171+marutigolande2022@users.noreply.github.com>
2024-04-03 22:10:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,37 @@ resource "aws_instance" "my_vm" {
 
 resource "aws_eip" "lb" {
   instance = aws_instance.my_vm.id
-  domain   = var.domain
+  domain   = var.sg_name
 }
 
+
+resource "aws_security_group" "example" {
+  name        = var.sg_name
+  description = "Allow inbound traffic on port 80 and 22"
+  vpc_id = aws_vpc.main.id  # Reference the ID of the VPC created above
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.ingress1_cidr_ipv4  # Allow traffic from any source on port 80
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.ingress2_cidr_ipv4  # Allow traffic from any source on port 22 (SSH)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all outbound traffic
+    cidr_blocks = var.egress1_cidr_blocks
+  }
+
+  tags = {
+    Name = "security-group"
+  }
+}
