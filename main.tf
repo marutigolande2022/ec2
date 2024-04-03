afe6aeaaf03e0043cb68c9771e2f0b2bd3b932a9
marutigolande2022 <163817171+marutigolande2022@users.noreply.github.com>
2024-04-03 22:22:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,15 @@ resource "aws_ami_from_instance" "example" {
   name               = "terraform-example"
   source_instance_id = "i-xxxxxxxx"
 }
+
+
+resource "aws_ami_copy" "example" {
+  name              = "terraform-example"
+  description       = "A copy of ami-xxxxxxxx"
+  source_ami_id     = "ami-xxxxxxxx"
+  source_ami_region = "us-west-1"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
