2b9e0ec7a30c36dfc4272263c6e72eac17cb39a4
marutigolande2022 <163817171+marutigolande2022@users.noreply.github.com>
2024-04-03 22:12:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,3 +17,28 @@ variable "domain" {
    type        = string
    description = "Name of the EC2 instance"
 }
+
+variable "sg_name" {
+   type        = string
+   description = "Ubuntu AMI ID in N. Virginia Region"
+}
+
+variable "ingress1_cidr_ipv4" {
+   type        = string
+   description = "Ubuntu AMI ID in N. Virginia Region"
+}
+
+variable "ingress2_cidr_ipv4" {
+   type        = string
+   description = "Ubuntu AMI ID in N. Virginia Region"
+}
+
+variable "ingress1_cidr_ipv6" {
+   type        = string
+   description = "Ubuntu AMI ID in N. Virginia Region"
+}
+
+variable "egress1_cidr_blocks" {
+   type        = string
+   description = "Ubuntu AMI ID in N. Virginia Region"
+}
