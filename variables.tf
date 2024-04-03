01f2b38f12994622ba83e6a077807fa87308c1e6
marutigolande2022 <163817171+marutigolande2022@users.noreply.github.com>
2024-04-03 22:17:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,2 @@
-variable "ami" {
-   type        = string
-   description = "Ubuntu AMI ID in N. Virginia Region"
-}
 
-variable "instance_type" {
-   type        = string
-   description = "Instance type"
-}
-
-variable "name_tag" {
-   type        = string
-   description = "Name of the EC2 instance"
-}
 
