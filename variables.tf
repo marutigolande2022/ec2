4e7c940ece1e3bc374643130dfc22fb418af0671
marutigolande2022 <163817171+marutigolande2022@users.noreply.github.com>
2024-04-04 14:44:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,2 +1,10 @@
+variable "instance_name" {
+   type        = string
+   description = "Ubuntu AMI ID in N. Virginia Region"
+}
 
+variable "source_instance_id" {
+   type        = string
+   description = "Ubuntu AMI ID in N. Virginia Region"
+}
 
